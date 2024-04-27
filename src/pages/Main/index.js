034a68e953f34255b94f8af0a1600f175bc32c42
Samilly <samillynunes19@gmail.com>
2024-04-27 19:54:32 -0300

    ui: animate icon and other minor things

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import { useState, useCallback } from "react";
-import { FaGithub, FaPlus } from "react-icons/fa";
+import { FaGithub, FaPlus, FaSpinner } from "react-icons/fa";
 
 import { Container, Form, SubmitButton } from "./styles";
 import api from "../../services/api";
@@ -9,6 +9,7 @@ export default function Main(){
 
     const [newRepo, setNewRepo] = useState('');
     const [repositories, setRepositories] = useState('');
+    const [loading, setLoading] = useState(false);
 
     function handleInputChange(e){
         setNewRepo(e.target.value);
@@ -19,18 +20,28 @@ export default function Main(){
         e.preventDefault();
 
         async function submit(){
-            const response = await api.get(`repos/${newRepo}`);
-    
-            const data = {
-                name: response.data.full_name,
+            setLoading(true);
+            try {
+                
+                const response = await api.get(`repos/${newRepo}`);
+        
+                const data = {
+                    name: response.data.full_name,
+                }
+        
+                setRepositories([...repositories, data]);
+        
+                setNewRepo('');
+        
+                console.log(repositories);
+                
+            } catch (error) {
+                console.log(error);
+            } finally{
+                
+                setLoading(false);
             }
-    
-            setRepositories([...repositories, data]);
-    
-            setNewRepo('');
-    
-            console.log(repositories);
-            
+
         }
 
         submit();
@@ -53,8 +64,13 @@ export default function Main(){
                         onChange={handleInputChange}
                     />
 
-                    <SubmitButton>
-                        <FaPlus color="#FFF" size={14} />
+                    <SubmitButton loading={loading} >
+                        {loading ? (
+                            <FaSpinner color="#FFF" size={14} />
+                        ) : (
+                            <FaPlus color="#FFF" size={14} />
+
+                        )}
                     </SubmitButton>
                 </Form>
             </Container>

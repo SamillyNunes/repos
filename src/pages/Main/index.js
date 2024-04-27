530df618bd15312bd802968e6d5a9ce7b8d8409e
Samilly <samillynunes19@gmail.com>
2024-04-27 17:45:45 -0300

    added react icons lib and styling main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,25 @@
-import { Title } from "./styles";
+import { FaGithub, FaPlus } from "react-icons/fa";
+
+import { Container, Form, SubmitButton } from "./styles";
 
 
 export default function Main(){
     return (
         <div>
-            <Title>Main</Title>
+            <Container>
+                <h1>
+                    <FaGithub size={25} />
+                    Meus repositórios
+                </h1>
+
+                <Form onSubmit={()=>{}}>
+                    <input type="text" placeholder="Adicionar Repositorios" />
+
+                    <SubmitButton>
+                        <FaPlus color="#FFF" size={14} />
+                    </SubmitButton>
+                </Form>
+            </Container>
         </div>
     );
 }

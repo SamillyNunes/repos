d33673d37e74dfe36938bf77f2a9e2983f3e3a60
Samilly <samillynunes19@gmail.com>
2024-04-29 18:06:54 -0300

    feat: getting repo info and issues

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,11 +1,44 @@
-export default function Repository({match}){
-    return (
-        <div>
-            <h1 style={{ color: '#FFF' }} >
-                Repository 
-                {decodeURIComponent(match.params.repository)}
-            </h1>
+import { useState, useEffect } from "react";
 
-        </div>
+import { Container } from "./styles";
+import api from "../../services/api";
+
+export default function Repository({match}){
+
+    const [repository, setRepository] = useState({});
+    const [issues, setIssues] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        async function loadRepo(){
+            const nomeRepo = decodeURIComponent(match.params.repository);
+
+            const [repoData, issuesData] = await Promise.all([
+                api.get(`/repos/${nomeRepo}`),
+                api.get(`/repos/${nomeRepo}/issues`, {
+                    params: {
+                        state: 'open',
+                        per_page:5
+                    }
+                }),
+            ]);
+
+            console.log(repoData.data);
+            console.log(issuesData.data);
+
+            setRepository(repoData.data);
+            setIssues(issuesData.data);
+
+            setLoading(false);
+
+        }
+
+        loadRepo();
+    },[match.params.repository]);
+
+    return (
+        <Container>
+
+        </Container>
     );
 }

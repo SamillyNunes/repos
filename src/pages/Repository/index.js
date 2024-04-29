4069729f250fe2e678fd4752dd26962265f09ae7
Samilly <samillynunes19@gmail.com>
2024-04-29 18:40:20 -0300

    ui: listing issues

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-import { Container, Owner, Loading, BackButton } from "./styles";
+import { Container, Owner, Loading, BackButton,IssuesList } from "./styles";
 import api from "../../services/api";
 import { FaArrowLeft } from "react-icons/fa";
 
@@ -53,6 +53,27 @@ export default function Repository({match}){
                 <h1> {repository.name} </h1>
                 <p> {repository.description} </p>
             </Owner>
+
+            <IssuesList>
+                {issues.map(issue=>(
+                    <li key={String(issue.id)}>
+                        <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+                        <div>
+                            <strong>
+                                <a href={issue.html_url}> {issue.title} </a>
+
+                                {issue.labels.map(label => (
+                                    <span key={String(label.id)} > {label.name} </span>
+                                ))}
+                            </strong>
+                            
+                            <p> {issue.user.login} </p>
+                        </div>
+                    </li>
+                ))}
+            </IssuesList>
+
         </Container>
     );
 }
